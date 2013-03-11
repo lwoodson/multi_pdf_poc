250e64bef79f5306260bb0439f142b2eb98f0a36
Lance Woodson <lance.woodson@peopleadmin.com>
2013-03-11 07:13:25 -0500

    Initial

---
+++ pdfjs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdfjs::Application.config.secret_token = 'f3086fa27db188bff946d57c258808acf4a05a5418812588c4b8e8953962f1790769c59d7fe3e7efd65c54c80aff2d2cae7d39bb517c3b829c765cd1b3acc7df'
